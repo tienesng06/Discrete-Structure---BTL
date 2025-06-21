37aaf03e592aa9e8a7029635dd96d2f98c7eb3fe
tienesng06 <96417925+tienesng06@users.noreply.github.com>
2025-06-21 10:32:50 +0700

    Update tsm.h

--- tsm.h
+++ tsm.h
@@ -1,5 +1,18 @@
-#include "main.h"
+#ifndef _BELLMAN_H_
+#define _BELLMAN_H_
 
-void Traveling(const vector<vector<int>>& graph, int numEdges, char startVertex);
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <random>
+using namespace std;
+#define MAX 1000
+#define INF 1000000000
 
-void TravelingLarge(const vector<vector<int>>& graph, int numEdges, char startVertex);
+using namespace std;
+
+string Traveling(int edgeList[][3], int numEdge, char startChar);
+#endif //_BELLMAN_H_
